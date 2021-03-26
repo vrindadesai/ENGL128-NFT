4597031f2e4aa022284f60f2505810a06d9c71a4
Vrinda Desai <vrinda@email.unc.edu>
2021-03-26 18:52:27 -0400

    Created file for project!

---
+++ drawing.py
@@ -0,0 +1,14 @@
+"""My NFT contribution for ENGL 128!"""
+
+from turtle import Turtle, colormode, done
+
+def main() -> None:
+    """The entrypoint for my scene."""
+    vrinda: Turtle = Turtle()
+
+def draw_real_woman() -> None:
+    return
+
+def draw_fake_woman() -> None:
+    return
+
